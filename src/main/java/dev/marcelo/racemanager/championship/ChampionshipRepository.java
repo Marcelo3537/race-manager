5e1991b54d6118b2006e9cb1553318257c7f4b25
Marcelo <macunabo@emeal.nttdata.com>
2026-09-10 23:00:33 +0200

    feat: add championship repository

---
+++ src/main/java/dev/marcelo/racemanager/championship/ChampionshipRepository.java
@@ -0,0 +1,8 @@
+package dev.marcelo.racemanager.championship;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface ChampionshipRepository extends JpaRepository<Championship, Long> {
+
+    boolean existsByNameIgnoreCaseAndSeason(String name, Integer season);
+}
